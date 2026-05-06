a24203d4d9e0c9eb0042629449e0cfb7aa899810
Mayara Labidi <labidi.mayara1393@gmail.com>
2026-05-06 14:52:56 +0100

    fix

--- skillfortat-server/jest.config.js
+++ skillfortat-server/jest.config.js
@@ -1,4 +1,14 @@
 export default {
   testEnvironment: "node",
   transform: {},
+
+  collectCoverage: true,
+
+  collectCoverageFrom: [
+    "src/**/*.js"
+  ],
+
+  coverageDirectory: "coverage",
+
+  coverageReporters: ["lcov", "text"]
 };
